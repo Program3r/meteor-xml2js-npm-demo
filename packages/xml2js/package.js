4922d5656022d5a915acc57390a6351a88c75677
Avital Oliver <avital@thewe.net>
2013-11-02 08:23:01 -0700

    Update package.js

--- packages/xml2js/package.js
+++ packages/xml2js/package.js
@@ -5,7 +5,6 @@ Package.describe({
 Npm.depends({xml2js: "0.2.6"});
 
 Package.on_use(function (api) {
-  api.export('XML2JS');
-  
+  api.export('XML2JS'); // `api.export` introduced in Meteor 0.6.5
   api.add_files("xml2js.js", "server");
 });
