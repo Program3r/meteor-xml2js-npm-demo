5a0ad1b611503c585aa3ea460b3118547d9d0362
Avital Oliver <avital@thewe.net>
2013-11-02 08:21:33 -0700

    Eliminate backcompat for pre-0.6.5

--- packages/xml2js/package.js
+++ packages/xml2js/package.js
@@ -5,8 +5,7 @@ Package.describe({
 Npm.depends({xml2js: "0.2.6"});
 
 Package.on_use(function (api) {
-  if (api.export) // ensure backwards compatibility with Meteor pre-0.6.5
-    api.export('XML2JS');
-
+  api.export('XML2JS');
+  
   api.add_files("xml2js.js", "server");
 });
